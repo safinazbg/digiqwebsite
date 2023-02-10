320c12a77733a145e9f3bb334f802d0a773e8677
Safinaz Büyükgüzel <42682032+safinazbg@users.noreply.github.com>
2023-02-10 03:45:15 +0100

    relative production paths

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,5 @@ const path = require('path')
 module.exports = {
   runtimeCompiler: true,
   outputDir: path.resolve(__dirname, "./docs"),
-  publicPath: process.env.NODE_ENV === 'production'
-    ? ''
-    : './'
+  publicPath: '/'
 };
